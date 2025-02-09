5149d0898d7ac18ed3f2ca282acf0903b9510ca9
Dennis Mullen <dkmullen@gmail.com>
2025-02-08 21:27:29 -0500

    Accordian, Buttons, etc

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,31 @@
 import BaseSelect from './components/BaseSelect.vue'
 import BaseInput from './components/BaseInput.vue'
 import AccordianView from './components/AccordianView.vue'
+import BaseButton from './components/BaseButton.vue'
 
 const items = ['Uno', 'Dos', 'Tres']
+const accordianItems = [
+  { title: 'Check One', bodyText: '<strong>Item one</strong> is a good one.' },
+  { title: 'Check Two', bodyText: '<strong>Item 2</strong> is a good one.' },
+  { title: 'Check Three', bodyText: '<strong>Item 3</strong> is a good one.' }
+]
+function logButtonMsg(message) {
+  console.log(message)
+}
 </script>
 
 <template>
   <main>
-    <BaseSelect :items="items" @val="showMe" label="Pick one" />
+    <BaseSelect :items="items" label="Pick one" />
     <BaseInput label="Email" />
-    <AccordianView />
+    <AccordianView :items="accordianItems" id="myAccordian" />
+    <BaseButton label="Click Me" color="success" @click="logButtonMsg('Success!')" />
+    <BaseButton
+      label="Wha?"
+      color="primary"
+      :outline="true"
+      @click="logButtonMsg('Wha?')"
+      icon="alarm"
+    />
   </main>
 </template>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+  type: { type: String, default: 'button' },
+  label: { type: String, required: true },
+  color: { type: String, default: 'primary' },
+  outline: { type: Boolean, default: false },
+  icon: { type: String }
+})
+const emit = defineEmits(['click'])
+</script>
+\
+<template>
+  <button
+    class="btn"
+    :class="[props.outline ? `btn-outline-${props.color}` : `btn-${props.color}`]"
+    @click="emit('click')"
+  >
+    <i v-if="props.icon" class="me-1 bi" :class="`bi-${icon}`"></i>
+    {{ props.label }}
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  margin: 1rem 4px 1rem;
+}
+</style>
+
+<!-- Icon list at https://icons.getbootstrap.com/ -->
